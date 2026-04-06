378e75424a047112c63eda44c927121a9802fc4c
MuhammedFaseehBetter <mhfaseeh14@gmail.com>
2026-04-06 14:36:19 -0500

    testing and removing tests

--- JetsonLocal/tests/test_tts_agent.py
+++ JetsonLocal/tests/test_tts_agent.py
@@ -1,9 +1,16 @@
+from pathlib import Path
+import sys
+
+BASE_DIR = Path(__file__).resolve().parents[1]
+if str(BASE_DIR) not in sys.path:
+    sys.path.insert(0, str(BASE_DIR))
+
 from agent.tts import TTSService
+
 
 if __name__ == "__main__":
     tts = TTSService(
         voice="gmw/en-us",
-        device="hw:1,0"  # change if needed
+        device="hw:1,0",
     )
-
     tts.speak("Hello. Aura text to speech is now integrated.")
